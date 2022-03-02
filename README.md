2df2a336d75b1e910735320bc836aba4cf39e51a
elianajn <elianajneu@gmail.com>
2022-03-01 18:13:21 -0700

    workinggit add *

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Preference Testing
-### Mir Qi and Carlton Moeller
+### Mir Qi, Carlton Moeller, and Ella Neurohr
 **Current status:**
 - [ ] input reading
   - [x] sound files
@@ -7,9 +7,9 @@
   - [ ] error catching for invalid files
   - [x] animal ID
 - [x] CSV output
-  - [ ] confirm datetime and elapsed time output is correct once on Rpi
+  - [x] confirm datetime and elapsed time output is correct once on Rpi
 - [ ] auto pushing CSV files
 - [ ] test pin rising/falling trigger
 - [ ] GIOP testing
-- [ ] sound output testing
+- [x] sound output testing
 - [ ] in code documentation
--- preference_test_final.py
+++ preference_test_final.py
@@ -3,76 +3,85 @@ from datetime import datetime
 import time
 import csv
 import pygame as pg
+import keyboard
 
 # arr = np.empty()
 arr = []
+start_time = None
 fam_sound = ""
 unfam_sound = ""
 
 
 GPIO.setmode(GPIO.BOARD)  # set up GPIO pin numeration to ordinal
-GPIO.setup(11, GPIO.IN) 
-GPIO.setup(15, GPIO.IN)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# print(familiar sound name?)
+'''
+Prompts the user for full file paths to the sound files
+TODO: check file type?
+'''
 def get_sound_files():
     global fam_sound
     global unfam_sound
-    fam_sound = input("Familiar sound file name: ")
+    fam_sound = input("Familiar sound file path: ")
     print(str(fam_sound) + " will be used as the familiar sound")
-    unfam_sound = input("Unfamiliar sound file name: ")
+    unfam_sound = input("Unfamiliar sound file path: ")
     print(str(unfam_sound) + " will be used as the familiar sound")
+    fam_sound = "/home/pi/Desktop/birb/Lab1_bird3.wav"
+    unfam_sound = "/home/pi/Desktop/birb/Lab1_bird7.wav"
 
-
+'''
+Plays sound, cuts off playback if method is called again before the sound finishes playing
+'''
 def play_sound(sound):
     pg.mixer.init()
-    pg.mixer.music.load(sound)  # Load go sound
+    pg.mixer.music.load(sound)  # Load sound
     pg.mixer.music.play()
-    while pg.mixer.music.get_busy():  # Keep on hold while playing
-        continue
-    pg.mixer.quit()
-
-
+    
+    
 '''
-https://raspi.tv/2014/rpi-gpio-update-and-detecting-both-rising-and-falling-edges
+Callback function for channel 11
+Appends to data array if the beam is broken or unbroken
+'''
+def callback_beam_11(channel):
+    global arr, start_time
+    now = datetime.now().strftime("%H:%M:%S.%f")
+    if GPIO.input(11):
+        print("11 beam unbroken")
+        arr.append([now, time.time() - start_time, 0, 0]) 
+    else:
+        print("11 beam broken")
+        arr.append([now, time.time() - start_time, 1, 0])
+        play_sound(fam_sound)
+    
+'''
+Callback function for channel 15
+Appends to data array if the beam is broken or unbroken
+'''
+def callback_beam_15(channel):
+    global arr, start_time
+    now = datetime.now().strftime("%H:%M:%S.%f")
+    if GPIO.input(15):
+        print("15 beam unbroken")
+        arr.append([now, time.time() - start_time, 0, 0])
+    else:
+        print("15 beam broken")
+        arr.append([now, time.time() - start_time, 0, 1])
+        play_sound(unfam_sound)
+    
+'''
 '''
 def listen(duration):
-    global arr
+    global start_time
     start_time = time.time()
-    print(fam_sound)
-    #GPIO.add_event_detect(15, GPIO.BOTH)
-    #while ((start_time.strftime("%H") - time_now.strftime("%H"))*60*60 +(start_time.strftime("%M") - time_now.strftime("%M"))*60 + (start_time.strftime("%S") - time_now.strftime("%S")) )*1000+ (start_time.strftime("%f") - time_now.strftime("%f")) < duration:
-    print(fam_sound)
-    GPIO.add_event_detect(11, GPIO.FALLING)
+    GPIO.add_event_detect(11, GPIO.BOTH, callback=callback_beam_11)
+    GPIO.add_event_detect(15, GPIO.BOTH, callback=callback_beam_15)
+    #message = input("Press enter to quit\n\n") # uncomment for manual program termination
+    flag = True
     while time.time() - start_time < float(duration):
-        if GPIO.event_detected(11):
-            print((time.time() - start_time))
-            now = datetime.now().strftime("%H:%M:%S.%f")
-            if not GPIO.input(11):     # if port 11 == 1
-                play_sound(fam_sound)
-                arr.append([now, time.time() - start_time, True, False])  #TODO: make sure that this is what you want stored in respect to rising/falling
-                print("Falling edge detected on 11")
-                GPIO.remove_event_detect(11)
-                GPIO.add_event_detect(11, GPIO.RISING)
-            else:                  # if port 11 != 1  
-                arr.append([now, time.time() - start_time, False, False])
-                print("Rising edge detected on 11")
-                GPIO.remove_event_detect(11)
-                GPIO.add_event_detect(11, GPIO.FALLING)
-#        if GPIO.event_detected(15):
- #           print("hit 15")
-  #          now = datetime.now().strftime("%H:%M:%S.%f")
-   #         if GPIO.input(15):     # if port 15 == 1
-    #            play_sound(unfam_sound)
-     #           arr.append([now, time.time() - start_time, False, True])
-      #          print("Rising edge detected on 15")
-       #     else:                  # if port 15 != 1
-        #        arr.append([now, time.time() - start_time, False, False])
-         #       print("Rising edge detected on 15")
-    GPIO.remove_event_detect(11)
-    #GPIO.remove_event_detect(15)
-    GPIO.cleanup(11)
-    #GPIO.cleanup(15)
+        continue
+    GPIO.cleanup()
+    pg.mixer.quit()
 
 
 def main():
@@ -82,7 +91,7 @@ def main():
     while animal_id == "":
         animal_id = input("Must enter animal_id: ")
     duration = int(input("How many seconds would you like the session to run? ")) #*60
-    gotime = input("Ready?(y/n) ")
+    gotime = input("Ready? (y/n) ")
     if gotime == "y":
         listen(duration)
     filename = "{}.csv".format(animal_id)
